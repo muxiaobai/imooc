7723c8e3e8db0c2e1d0a80558968595f0a6213c0
慕小白 <zpf12345678910@gmail.com>
2016-08-06 22:21:24 +0800

    On branch master 	new file:   .babelrc 	new file:   .editorconfig 	new file:   .eslintrc 	new file:   .gitignore 	new file:   .yo-rc.json 	new file:   cfg/base.js 	new file:   cfg/defaults.js 	new file:   cfg/dev.js 	new file:   cfg/dist.js 	new file:   cfg/test.js 	new file:   dist/README.md 	new file:   karma.conf.js 	new file:   package.json 	new file:   server.js 	new file:   src/actions/README.md 	new file:   src/components/ContentComponent.js 	new file:   src/components/Main.js 	new file:   src/config/README.md 	new file:   src/config/base.js 	new file:   src/config/dev.js 	new file:   src/config/dist.js 	new file:   src/config/test.js 	new file:   src/data/imageData.json 	new file:   src/favicon.ico 	new file:   src/images/yeoman.png 	new file:   src/index.html 	new file:   src/index.js 	new file:   src/sources/README.md 	new file:   src/stores/README.md 	new file:   src/styles/App.css 	new file:   test/actions/.keep 	new file:   test/components/MainTest.js 	new file:   test/config/ConfigTest.js 	new file:   test/helpers/shallowRenderHelper.js 	new file:   test/loadtests.js 	new file:   test/sources/.keep 	new file:   test/stores/.keep 	new file:   webpack.config.js

---
+++ src/components/ContentComponent.js
@@ -0,0 +1,12 @@
+var ContentComponent = React.createClass{
+    render(){
+        return(
+        <section className="stage">
+            <section className="img-sec">
+            </section>
+            <nav className="controller-nav">
+            </nav>
+        </section>
+            );
+    }
+}
